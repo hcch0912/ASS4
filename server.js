063d3e3ad3c174ada83f54d22b90227639479cf6
veronicachen0912 <veroniachen0912@gmail.com>
2016-04-24 10:36:21 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,71 @@
+//dependencies for each module used
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var handlebars = require('express-handlebars');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var dotenv = require('dotenv');
+var pg = require('pg');
+var app = express();
+
+//client id and client secret here, taken from .env (which you need to create)
+dotenv.load();
+
+//connect to database
+var conString = process.env.DATABASE_CONNECTION_URL;
+
+//Configures the Template engine
+app.engine('html', handlebars({ defaultLayout: 'layout', extname: '.html' }));
+app.set("view engine", "html");
+app.set('views', __dirname + '/views');
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(session({ secret: 'keyboard cat',
+                  saveUninitialized: true,
+                  resave: true}));
+
+//set environment ports and start application
+app.set('port', process.env.PORT || 3000);
+
+//routes
+app.get('/', function(req, res){
+  res.render('index');
+});
+
+app.get('/delphidata', function (req, res) {
+ var results=[];
+
+    pg.connect(conString, function(err, client, done) {
+        // Handle connection errors
+        if(err) {
+          done();
+          console.log(err);
+          return res.status(500).json({ success: false, data: err});
+        }
+
+        // SQL Query > Select Data
+        var query = client.query("SELECT * FROM cogs121_16_raw.hhsa_san_diego_demographics_rental_statistics_2012");
+
+        // Stream results back one row at a time
+        query.on('row', function(row) {
+            results.push(row);
+        });
+
+        // After all data is returned, close connection and return results
+        query.on('end', function() {
+            done();
+            return res.json(results);
+        });
+
+    });
+
+
+  return { delphidata: "No data present." }
+});
+
+
+http.createServer(app).listen(app.get('port'), function() {
+    console.log('Express server listening on port ' + app.get('port'));
+});
