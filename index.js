f384f3be89e1b62a0716d0d051506a0ba14b0655
veronicachen0912 <veroniachen0912@gmail.com>
2016-04-27 07:58:41 -0700

    add pie chart,but only a sample,still need the name of area when clicking the mark.BTW,looks terrible.

--- index.js
+++ index.js
@@ -1,201 +1,109 @@
+
 (function(d3) {
   "use strict";
 
-  var datas={"labels":[],
-            "series":[
-            {
-              "labels":[],
-              "values":[]
-            },
-            {
-              "labels":[],
-              "values":[]
-            },
-            {
-              "labels":[],
-              "values":[]
-            }                        
-            ]
-  };
-d3.json("/delphidata", function(err, data) {
-
-    if (err) {
-      console.log(err);
-      return;
-    }
-
-    var areaArr=[];
-    var priceArr1=[[],[],[]];
-    for(var i=0;i<data.length;i++){
-      areaArr[i]=data[i].Area;
-      
-        priceArr1[0][i]=data[i]['Rent <$500'];
-        priceArr1[1][i]=data[i]['Rent $1-$1.3K'];
-        priceArr1[2][i]=data[i]['Rent $2K+'];
-      
-    }
-   datas.labels=areaArr;
-   datas.series.labels=["Rent <$500","Rent $1-1.3K","Rent $2K+"];
-   datas.series[0].values=priceArr1[0];
-   datas.series[1].values=priceArr1[1];
-   datas.series[2].values=priceArr1[2];
+d3.json("/delphidata", function(err, resData) {//resData is Array with 42 json element
 
 
+//all
+var dataset=[];
+
+    for(var i=0;i<resData.length;i++){
+          var data=[]; 
+    
+          data[0]={"price":"Rent <$700","number":resData[i]['Rent <$500']+resData[i]['Rent $500-$599']+resData[i]['Rent $600-$699']}; 
+          data[1]={"price":"Rent $700-$1K","number":resData[i]['Rent $700-$799']+resData[i]['Rent $800-$899']+resData[i]['Rent $900-$999']};
+          data[2]={"price":"Rent $1-$1.3K","number":resData[i]['Rent $1-$1.3K']};
+          data[3]={"price":"Rent $1.3-$1.5K","number":resData[i]['Rent $1.3-$1.5K']};
+          data[4]={"price":"Rent $1.5-$2K","number":resData[i]['Rent $1.5-$2K']};
+          data[5]={"price":"Rent $2K+","number":resData[i]['Rent $2K+']};
+          var ajson={"Area":resData[i].Area,"Data":data};
+          dataset.push(ajson);
+    };
+  //from light to dark
+var color=["#e6f5ff","#99d6ff","#4db8ff","#006bb3","#0030cc","#001866"]; 
+var colorAreaArr=[];
+for(var i=0;i<dataset.length;i++){
+    var thisColor;
+    var max;//the majority interval 
+    var tempArr=[];
+    for(var j=0;j<6;j++){ 
+        tempArr[j]=dataset[i].Data[j].number;  
+    };
+    max=Math.max(...tempArr);
+    for(var j=0;j<6;j++){ 
+        if(dataset[i].Data[j].number==max){
+          thisColor=color[j];
+        };  
+    };
+    colorAreaArr.push({"Area":dataset[i].Area,"Color":thisColor});
+};
+
+
+//choose area
+var chooseArea=function(area,result){
+      for(var i=0;i<dataset.length;i++){
+        if(area==dataset[i].Area){
+          result=dataset[i].Data;
+        }
+      }
+      return result;
+};
+
+
+var width = 960,
+    height = 500,
+    radius = Math.min(width, height) / 2;
+
+var color = d3.scale.category20();
+
+var arc = d3.svg.arc()
+    .outerRadius(radius - 10)
+    .innerRadius(0);
+
+var labelArc = d3.svg.arc()
+    .outerRadius(radius - 40)
+    .innerRadius(radius - 40);
+
+var pie = d3.layout.pie()
+    .sort(null)
+    .value(function(d) { return d.number; });
+
+var svg = d3.select("body").append("svg")
+    .attr("width", width)
+    .attr("height", height)
+  .append("g")
+    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
 
 
- 
- var dataSeriesSize=3;
+  var g = svg.selectAll(".arc")
+      .data(pie(dataset[0].Data))
+    .enter().append("g")
+      .attr("class", "arc");
 
-var chartWidth       = 300,
-    barHeight        = 20,
-    groupHeight      = barHeight * dataSeriesSize,
-    gapBetweenGroups = 10,
-    spaceForLabels   = 150,
-    spaceForLegend   = 150;
+  g.append("path")
+      .attr("d", arc)
+      .style("fill", function(d,i) { return color(i%d.data.number); });
 
-// Zip the series data together (first values, second values, etc.)
-var zippedData = [];
-for (var i=0; i<datas.labels.length; i++) {
-  for (var j=0; j<dataSeriesSize; j++) {
-    zippedData.push(datas.series[j].values[i]);
-  }
+  g.append("text")
+      .attr("transform", function(d) { return "translate(" + labelArc.centroid(d) + ")"; })
+      .attr("dy", ".35em")
+      .text(function(d) {return d.data.price; });
+
+  // var text=document.getElementById("h1");
+  //   text.innerHTML=d.
+
+
+function type(d) {
+  d.number = +d.number;
+  return d;
 }
 
-// Color scale
-var color = d3.scale.category20();
-var chartHeight = barHeight * zippedData.length + gapBetweenGroups * datas.labels.length;
 
-var x = d3.scale.linear()
-    .domain([0, d3.max(zippedData)])
-    .range([0, chartWidth]);
-
-var y = d3.scale.linear()
-    .range([chartHeight + gapBetweenGroups, 0]);
-
-var yAxis = d3.svg.axis()
-    .scale(y)
-    .tickFormat('')
-    .tickSize(0)
-    .orient("left");
-
-// Specify the chart area and dimensions
-var chart = d3.select(".chart")
-    .attr("width", spaceForLabels + chartWidth + spaceForLegend)
-    .attr("height", chartHeight);
-
-// Create bars
-var bar = chart.selectAll("g")
-    .data(zippedData)
-    .enter().append("g")
-    .attr("transform", function(d, i) {
-      return "translate(" + spaceForLabels + "," + (i * barHeight + gapBetweenGroups * (0.5 + Math.floor(i/dataSeriesSize))) + ")";
-    });
-
-// Create rectangles of the correct width
-bar.append("rect")
-    .attr("fill", function(d,i) { return color(i % dataSeriesSize); })
-    .attr("class", "bar")
-    .attr("width", x)
-    .attr("height", barHeight - 1);
-
-// Add text label in bar
-bar.append("text")
-    .attr("x", function(d) { return x(d) - 3; })
-    .attr("y", barHeight / 2)
-    .attr("fill", "red")
-    .attr("dy", ".35em")
-    .text(function(d) { return d; });
-
-// Draw labels
-bar.append("text")
-    .attr("class", "label")
-    .attr("x", function(d) { return - 10; })
-    .attr("y", groupHeight / 2)
-    .attr("dy", ".35em")
-    .text(function(d,i) {
-      if (i % dataSeriesSize === 0)
-        return datas.labels[Math.floor(i/dataSeriesSize)];
-      else
-        return ""});
-
-chart.append("g")
-      .attr("class", "y axis")
-      .attr("transform", "translate(" + spaceForLabels + ", " + -gapBetweenGroups/2 + ")")
-      .call(yAxis);
-
-// Draw legend
-var legendRectSize = 18,
-    legendSpacing  = 4;
-
-var legend = chart.selectAll('.legend')
-    .data(datas.series)
-    .enter()
-    .append('g')
-    .attr('transform', function (d, i) {
-        var height = legendRectSize + legendSpacing;
-        var offset = -gapBetweenGroups/2;
-        var horz = spaceForLabels + chartWidth + 40 - legendRectSize;
-        var vert = i * height - offset;
-        return 'translate(' + horz + ',' + vert + ')';
-    });
-
-legend.append('rect')
-    .attr('width', legendRectSize)
-    .attr('height', legendRectSize)
-    .style('fill', function (d, i) { return color(i); })
-    .style('stroke', function (d, i) { return color(i); });
-
-legend.append('text')
-    .attr('class', 'legend')
-    .attr('x', legendRectSize + legendSpacing)
-    .attr('y', legendRectSize - legendSpacing)
-    .text(function (d) { return d.label; });
-   
- });
-
+});
 
 })(d3);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,4 @@
+
 var map;
 
 $(document).ready(function() {
@@ -15,177 +16,84 @@ $(document).ready(function() {
   })
 });
 
-/*function initMap() {
-  map = new google.maps.Map(document.getElementById('jsmap'), {
-    center: {lat: 32.842674, lng: -117.157767},
-    zoom: 11
-  });
-  console.log("aaa");
 
-  // place markers on the map
-  for(i in location) {
-    console.log(location[i].name);
-  }
 
-};*/
-
-(function(d3) {
+ function  renderPie(areaName) {
   "use strict";
+ d3.json("/delphidata", function(err, resData) {
+ var dataset=[];
+ var thisArea;
+    for(var i=0;i<resData.length;i++){
+          var data=[]; 
+    
+          data[0]={"price":"Rent <$700","number":resData[i]['Rent <$500']+resData[i]['Rent $500-$599']+resData[i]['Rent $600-$699']}; 
+          data[1]={"price":"Rent $700-$1K","number":resData[i]['Rent $700-$799']+resData[i]['Rent $800-$899']+resData[i]['Rent $900-$999']};
+          data[2]={"price":"Rent $1-$1.3K","number":resData[i]['Rent $1-$1.3K']};
+          data[3]={"price":"Rent $1.3-$1.5K","number":resData[i]['Rent $1.3-$1.5K']};
+          data[4]={"price":"Rent $1.5-$2K","number":resData[i]['Rent $1.5-$2K']};
+          data[5]={"price":"Rent $2K+","number":resData[i]['Rent $2K+']};
+          var ajson={"Area":resData[i].Area,"Data":data};
+          dataset.push(ajson);
+    };
+         
+    for(var i=0;i<dataset.length;i++){
+      if(dataset[i].Area==areaName){
+        console.log("success"+areaName);
+        thisArea=dataset[i];
+      }
+    };
+console.log(thisArea.Area);
 
-  var datas = {
-    "labels": [],
-    "series": [
-      {
-        "labels": [],
-        "values": []
-      },
-      {
-        "labels": [],
-        "values": []
-      },
-      {
-        "labels": [],
-        "values": []
-      }                        
-    ]
-  };
+  var width = 960,
+      height = 500,
+      radius = Math.min(width, height) / 2;
 
-d3.json("/delphidata", function(err, data) {
+  var color = d3.scale.category20();
 
-    if (err) {
-      console.log(err);
-      return;
-    }
+  var arc = d3.svg.arc()
+      .outerRadius(radius - 10)
+      .innerRadius(0);
 
-    var areaArr=[];
-    var priceArr1=[[],[],[]];
-    for(var i=0;i<data.length;i++){
-      areaArr[i]=data[i].Area;
-      
-        priceArr1[0][i]=data[i]['Rent <$500'];
-        priceArr1[1][i]=data[i]['Rent $1-$1.3K'];
-        priceArr1[2][i]=data[i]['Rent $2K+'];
-      
-    }
-   datas.labels=areaArr;
-   datas.series.labels=["Rent <$500","Rent $1-1.3K","Rent $2K+"];
-   datas.series[0].values=priceArr1[0];
-   datas.series[1].values=priceArr1[1];
-   datas.series[2].values=priceArr1[2];
+  var labelArc = d3.svg.arc()
+      .outerRadius(radius - 40)
+      .innerRadius(radius - 40);
 
-var dataSeriesSize=3;
+  var pie = d3.layout.pie()
+      .sort(null)
+      .value(function(d) { return d.number; });
 
-var chartWidth       = 300,
-    barHeight        = 20,
-    groupHeight      = barHeight * dataSeriesSize,
-    gapBetweenGroups = 10,
-    spaceForLabels   = 150,
-    spaceForLegend   = 150;
+  var svg = d3.select("body").append("svg")
+      .attr("width", width)
+      .attr("height", height)
+    .append("g")
+      .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
-// Zip the series data together (first values, second values, etc.)
-var zippedData = [];
-for (var i=0; i<datas.labels.length; i++) {
-  for (var j=0; j<dataSeriesSize; j++) {
-    zippedData.push(datas.series[j].values[i]);
+
+
+    var g = svg.selectAll(".arc")
+        .data(pie(thisArea.Data))
+      .enter().append("g")
+        .attr("class", "arc");
+
+    g.append("path")
+        .attr("d", arc)
+        .style("fill", function(d,i) { return color(i%d.data.number); });
+
+    g.append("text")
+        .attr("transform", function(d) { return "translate(" + labelArc.centroid(d) + ")"; })
+        .attr("dy", ".35em")
+        .text(function(d) {return d.data.price; });
+
+    // var text=document.getElementById("h1");
+    //   text.innerHTML=d.
+
+
+  function type(d) {
+    d.number = +d.number;
+    return d;
   }
+});
 }
-
-// Color scale
-var color = d3.scale.category20();
-var chartHeight = barHeight * zippedData.length + gapBetweenGroups * datas.labels.length;
-
-var x = d3.scale.linear()
-    .domain([0, d3.max(zippedData)])
-    .range([0, chartWidth]);
-
-var y = d3.scale.linear()
-    .range([chartHeight + gapBetweenGroups, 0]);
-
-var yAxis = d3.svg.axis()
-    .scale(y)
-    .tickFormat('')
-    .tickSize(0)
-    .orient("left");
-
-// Specify the chart area and dimensions
-var chart = d3.select(".chart")
-    .attr("width", spaceForLabels + chartWidth + spaceForLegend)
-    .attr("height", chartHeight);
-
-// Create bars
-var bar = chart.selectAll("g")
-    .data(zippedData)
-    .enter().append("g")
-    .attr("transform", function(d, i) {
-      return "translate(" + spaceForLabels + "," + (i * barHeight + gapBetweenGroups * (0.5 + Math.floor(i/dataSeriesSize))) + ")";
-    });
-
-// Create rectangles of the correct width
-bar.append("rect")
-    .attr("fill", function(d,i) { return color(i % dataSeriesSize); })
-    .attr("class", "bar")
-    .attr("width", x)
-    .attr("height", barHeight - 1);
-
-// Add text label in bar
-bar.append("text")
-    .attr("x", function(d) { return x(d) - 3; })
-    .attr("y", barHeight / 2)
-    .attr("fill", "red")
-    .attr("dy", ".35em")
-    .text(function(d) { return d; });
-
-// Draw labels
-bar.append("text")
-    .attr("class", "label")
-    .attr("x", function(d) { return - 10; })
-    .attr("y", groupHeight / 2)
-    .attr("dy", ".35em")
-    .text(function(d,i) {
-      if (i % dataSeriesSize === 0)
-        return datas.labels[Math.floor(i/dataSeriesSize)];
-      else
-        return ""});
-
-chart.append("g")
-      .attr("class", "y axis")
-      .attr("transform", "translate(" + spaceForLabels + ", " + -gapBetweenGroups/2 + ")")
-      .call(yAxis);
-
-// Draw legend
-var legendRectSize = 18,
-    legendSpacing  = 4;
-
-var legend = chart.selectAll('.legend')
-    .data(datas.series)
-    .enter()
-    .append('g')
-    .attr('transform', function (d, i) {
-        var height = legendRectSize + legendSpacing;
-        var offset = -gapBetweenGroups/2;
-        var horz = spaceForLabels + chartWidth + 40 - legendRectSize;
-        var vert = i * height - offset;
-        return 'translate(' + horz + ',' + vert + ')';
-    });
-
-legend.append('rect')
-    .attr('width', legendRectSize)
-    .attr('height', legendRectSize)
-    .style('fill', function (d, i) { return color(i); })
-    .style('stroke', function (d, i) { return color(i); });
-
-legend.append('text')
-    .attr('class', 'legend')
-    .attr('x', legendRectSize + legendSpacing)
-    .attr('y', legendRectSize - legendSpacing)
-    .text(function (d) { return d.label; });
-   
- });
-
-
-})(d3);
-
-
 
 
 
