976c2932e2f4bad6402d3dc4196e4cd67d04b1b5
Michelle Nguyen <mqn008@gmail.com>
2016-05-25 23:59:32 -0700

    fixed

--- public/js/map.js
+++ public/js/map.js
@@ -1,3 +1,19 @@
 $(document).ready( function () {
-  $('#dragme').draggable();
+	var closed = false;
+
+	$('#dragme').draggable();
+
+	$('#collapse').click(function() {
+		if (!closed ) {
+			$('#dragme').css('height','52px');
+			$('#dragme').css('width','52px');
+			closed=true;
+		}
+
+		else {
+			$('#dragme').css('height','auto');
+			$('#dragme').css('width','auto');
+			closed=false;
+		}
+	});
 });
